89a2686c5bd6231304530c58dad7cc4cd96437ac
Evlusha <106895767+Evlusha@users.noreply.github.com>
2024-04-29 23:27:40 +0800

    Update zd1.cpp

--- zd1.cpp
+++ zd1.cpp
@@ -1,81 +1,89 @@
-
-
+// Теорема Ферма и св-во сравнений(2 алгоритма)
 #include <iostream>
+#include <math.h>
+
 using namespace std;
-	// Проверка на простату 
-    bool is_prime(int p) {
-        if (p <= 1)
-            return false;
-        for (int i = 2; i * i <= p; ++i) {
-            if (p % i == 0)
-                return false;
-        }
-        return true;
-    }
-    // Через двоичный вид (a ^ x) % p
-    int bin_exp(long base, int exp, int mod) {
-        long res = 1;
-        while (exp) {
-            if (exp % 2 == 1) {
-                res *= base;
-                res %= mod;
-            }
-            base *= base;
-            base %= mod;
-            exp /= 2;
-        }
-        cout << "Ответ " << res << endl;
-        return res;
+
+bool CheckPrime(int p) { // проверка, является ли модуль простым числом
+    
+    if (p <= 1) return false; 
+    for (int i = 2; i * i <= p; ++i) { 
+        if (p % i == 0) return false;
     }
 
+    return true;
+}
 
-    // Функция для нахождения (a ^ x) % p
-    int mod_exp(int a, int x, int p) {  
-        int result = 1;
-        a = a % p;
-        while (x > 0) {
-            if (x % 2 == 1)
-                result = (result * a) % p;
-            x = x / 2;
-            a = (a * a) % p;            
-        }   
-        cout << "Ответ " << result << endl;
+int algorithm1(int a, int x, int p) { // Алгоритм1 через уменьшение степени
 
-        return bin_exp(a, x, p);
-        return result;
-       
+    int result = 1;
+    a %= p;
+    while (x > 0) {
+        if (x % 2 == 1)
+            result = (result * a) % p;
+        x /= 2;
+        a = (a * a) % p;
+    }
+    
+    return result;
+}
+
+int algorithm2(int a, int x, int p) { // Алгоритм2 через логарифм
+
+    int nums[100]; // инициализируем массив чисел, в котором хранится числовой ряд a
+    int stepen = 1; // для проверки, чтобы степень не превышала себя
+    nums[0] = a % p;
+
+    int n=1;
+
+    while (stepen <= x) { // вычисляем числовой ряд для числа a
+        stepen *= 2;
+        nums[n] = (nums[n-1] * nums[n-1]) % p; 
+        n += 1;
     }
 
-    // Проверка условий теоремы Ферма
-    bool fermat_theorem(int a, int p, int x) {
-        if (!is_prime(p)) 
-            return false;
-        if (mod_exp(a, x, p) == 1)
-            return true;
-        else
-            return false;
+    int binary[100]; // инициализируем массив чисел, в котором хранится число в двоичной степени
+    n = 0;
+    while (x != 1) { // переводчим степень в двоичное число
+        binary[n] = x % 2;
+        x /= 2;
+        n += 1;
+    }
+    binary[n] = x;
+    n += 1;
+
+    int result = 1;
+    for (int i = 0; i < n; ++i) { // используем конечную формулу алгоритма
+        result *= pow(nums[i], binary[i]);
+    }
+    result %= p;
+
+    return result;
+}
+
+int main(){
+    setlocale(LC_ALL, "Rus");
+
+    cout << "Нахождение остатка числа a^x mod p:" << endl;
+    
+    int a, x, p;
+    cout << "Введите числа a, x и p: ";
+    cin >> a >> x >> p;
+
+    if (CheckPrime(p)==false) { // проверка, является ли модуль простым числом
+        cout << "p не простое число." << endl;
+        return 1;
     }
 
-    int main() {
-        setlocale(LC_ALL, "Rus");
-        // Ввод a и p и x
-        int a;
-        cout << "Введите целое число a: ";
-        cin >> a;
-
-        int x;
-        cout << "Введите степнь x: ";     
-        cin >> x;
-
-        int p;         
-        cout << "Введите mod p: ";
-        cin >> p;
-        
-        if (fermat_theorem(a, p , x ))
-            cout << "Условия теоремы Ферма выполнены"<< endl;
-        else
-            cout << "Условия теоремы Ферма не выполнены " << endl;    
-
-        return 0;
+    if (a % p == 0 && x != 0) { // проверка, выполняется ли теорема Ферма
+        cout << "Теорема Ферма не выполняется: a^x ≡ 0 (mod p)" << endl;
+        return 1;
     }
 
+    int result1 = algorithm1(a, x, p);
+    cout << "Алгоритм 1: " << a << "^" << x << " mod " << p << " = " << result1 << endl;
+    int result2 = algorithm2(a, x, p);
+    cout << "Алгоритм 2: " << a << "^" << x << " mod " << p << " = " << result2 << endl;
+
+    return 0;
+}
